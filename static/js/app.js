3051e5d74bdf91658ee909dd0d37c2489d5cfe75
Morgan <44606382+recneps2@users.noreply.github.com>
2021-02-20 08:32:35 -0500

    <updating app>

--- static/js/app.js
+++ static/js/app.js
@@ -1,80 +1,53 @@
-function dropDown() {
-    var dropDownID = d3.select("#selDataset");
-    d3.json("samples.json").then((data) => {
-     // console.log(data);
-     var sampleIDs = data.names;
-     // console.log(sampleIDs);
-     sampleIDs.forEach((sampleValue) => {
-         dropDownID.append("option")
-         .text(sampleValue)
-         .property("value", sampleValue);
-     });
- var sampleID  = sampleIDs[0];
- // console.log(sampleID)
- metaData(sampleID);
- chart(sampleID);
- });
- 
- }
- dropDown();
- 
- function optionChanged(sample) {
-     metaData(sample);
-     chart(sample);
-     constructGauge(sample);
- };
- 
- function metaData(sampleID) {
-     d3.json("samples.json").then((data) => {
-         // console.log(data);
-         var metaIDs = data.metadata; 
-         // console.log(metaIDs.id);
-         // for (var i = 0; i < metaIDs.length; i++) {
-             // console.log(metaIDs[i])
-             var filterID = metaIDs.filter(object => object.id == sampleID);
-             // console.log(filterID);
-             var results = filterID[0];
-             var display = d3.select("#sample-metadata");
-             display.html("");
-             Object.entries(results).forEach(([key, value]) => {
-                 display.append("h6").text(`${key} ${value}`);
-             });
-             gauge(results.wfreq);
- 
-     });
- };
- 
- function chart(sampleID) {
-         d3.json("samples.json").then((data) => {
-         var samples = data.samples; 
-         var filterID = samples.filter(object => object.id == sampleID);
-             console.log(filterID);
-         var results = filterID[0];
-         var otuIDs = results.otu_ids;
-         var sampleValues = results.sample_values;
-         var otuLabels = results.otu_labels;
-         var barData = [{
-             x: sampleValues.slice(0, 10).reverse(),
-             y: otuIDs.slice(0, 10).map(otu_ID => `otuID ${otu_ID}`).reverse(),
-             text: otuLabels.slice(0, 10).reverse(),
-             type: "bar", 
-             orientation: "h"
-         }];
-         Plotly.newPlot("bar", barData)
-         var bubbleData = [{
-             x: otuIDs,
-             y: sampleValues,
-             text: otuLabels,
-             mode: "markers",
-             marker: {
-                 size: sampleValues,
-                 color: otuIDs,
-                 colorscale: "Earth" 
-             }
-         }];
-         Plotly.newPlot("bubble", bubbleData)
-     });
- 
- 
-         
- };
+function readFile(sample) {
+
+    d3.json('samples.json').then((data) => {
+        //console.log(data);
+        var names = data.names;
+        var metadata = data.metadata;
+        var samples = data.samples;
+        console.log(otulabels);
+        //var sample = '940'
+        console.log(sample);
+    
+        var dropdownMenu = d3.select("#selDataset");
+        names.forEach((ids) =>  {
+            dropdownMenu.append('option').property('value', ids).text(ids);
+        });
+    
+    
+        // sorted data
+    
+        // filtered the json data to the id referenced in line 10
+        var filtered_data = samples.filter(data => data.id == sample);
+        console.log(filtered_data);
+    
+        var results = filtered_data[0];
+    
+        var filtered_metadata = metadata.filter(metadata => metadata.id == sample);
+        console.log(filtered_metadata);
+    
+        var metadata_results = filtered_metadata[0];
+        var dem_id = d3.select("#sample-metadata");
+        dem_id.html('')
+        Object.entries(metadata_results).forEach(([key, value]) => {
+                dem_id.append('li').text(`${key}: ${value}`);
+    
+            });
+            
+    
+        //var samplevalues = samplesArray.sample_values;
+        //var sortedsamples = results.sort((a,b) =>
+        //b.sample - a.sample);
+    
+        //sliced to find top 10
+    
+        var topsamples = results.sample_values.slice(0,10).reverse();
+    
+        //reverse array to accomodate plotly's defaults
+    
+        //var reversed = sliced.reverse();
+        //var samples_values = sample_values.slice(0, 10).reverse()
+        var otulabels = results.otu_labels;
+        var otu_labels = otulabels.slice(0, 10).reverse()
+    
+        
